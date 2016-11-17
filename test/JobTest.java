1a80df529e65ed503f08fff1ba5006a75e13273f
Donal Maher <dpjm94@live.ie>
2016-11-17 12:25:40 +0000

    Added the getRange with throw exception.

--- test/JobTest.java
+++ test/JobTest.java
@@ -1,8 +1,14 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by donalmaher on 16/11/2016.
  */
 public class JobTest {
+    @Test
+    public void getRange() throws Exception {
+
+    }
 
 }
